8ec498afd3a599b0f67883d0d8d85341c6fbcf21
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-03-04 10:02:01 +0200

    Added JavaScript functionality and linked it to my HTML

--- budget.js
+++ budget.js
@@ -1,1 +1,59 @@
-/* www.youtube.com/CodeExplained */
+// Select Elements
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".outcome-total");
+const outcomeTotalEl = document.querySelector(".income-total");
+const chartEl = document.querySelector(".chart");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+
+// Buttons
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+// List 
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+// Input Buttons
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
+
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
+
+// Toggling
+
+// EVENT LISTENERS
+expenseBtn.addEventListener("click", function () {
+    show(expenseEl);
+    hide([incomeEl, allEl]);
+    active(expenseBtn);
+    inactive([incomeBtn, allBtn]);
+})
+incomeBtn.addEventListener("click", function () {
+    show(incomeEl);
+    hide([expenseEl, allEl]);
+    active(incomeBtn);
+    inactive([expenseBtn, allBtn]);
+})
+allBtn.addEventListener("click", function () {
+    show(allEl);
+    hide([incomeEl, expenseEl]);
+    active(allBtn);
+    inactive([incomeBtn, expenseBtn]);
+})
+
+
+// VARIABLES
+let ENTRY_LIST;
+let balance = 0, income = 0, outcome = 0;
+const DELETE = "delete", EDIT = "edit";
+
+// LOOK IF THERE IS SAVED DATA IN LOCALSTORAGE
+ENTRY_LIST = JSON.parse(localStorage.getItem("entry_list")) || [];
+updateUI();
